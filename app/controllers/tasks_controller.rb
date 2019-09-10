3430bde44c319d756f0fc5bb194370d87ced93fc
Tomoya-N <t.nakamurax@gmail.com>
2019-09-10 09:48:53 +0900

    add_create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,26 @@
 class TasksController < ApplicationController
+  before_action :move_to_index, except: :index
+
   def index
     @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    Task.create(task_name: task_params[:task_name], limit: task_params[:limit], user_id: current_user.id)
+    redirect_to(tasks_path)
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:task_name, :limit)
   end
 end
